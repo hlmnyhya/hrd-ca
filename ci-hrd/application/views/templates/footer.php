812dab8576c59d294b35e691befc59ce6e3d9352
AHjazi <ahmdhjzi06@gmail.com>
2023-04-09 03:01:31 +0800

    controller,pribadi,profile,karyawan

---
+++ ci-hrd/application/views/templates/footer.php
@@ -0,0 +1,39 @@
+ <!-- footer -->
+ <div class="container-fluid">
+     <div class="footer">
+         <p>Copyright © 2018 Designed by html.design. All rights reserved.<br><br>
+             Distributed By: <a href="https://themewagon.com/">ThemeWagon</a>
+         </p>
+     </div>
+ </div>
+ </div>
+ <!-- end dashboard inner -->
+ </div>
+ </div>
+ </div>
+ <!-- jQuery -->
+ <script src="<?php echo base_url();?>assets/js/jquery.min.js"></script>
+ <script src="<?php echo base_url();?>assets/js/popper.min.js"></script>
+ <script src="<?php echo base_url();?>assets/js/bootstrap.min.js"></script>
+ <!-- wow animation -->
+ <script src="<?php echo base_url();?>jassets/s/animate.js"></script>
+ <!-- select country -->
+ <script src="<?php echo base_url();?>assets/js/bootstrap-select.js"></script>
+ <!-- owl carousel -->
+ <script src="<?php echo base_url();?>assets/js/owl.carousel.js"></script>
+ <!-- chart js -->
+ <script src="<?php echo base_url();?>assets/js/Chart.min.js"></script>
+ <script src="<?php echo base_url();?>assets/js/Chart.bundle.min.js"></script>
+ <script src="<?php echo base_url();?>assets/js/utils.js"></script>
+ <script src="<?php echo base_url();?>assets/js/analyser.js"></script>
+ <!-- nice scrollbar -->
+ <script src="<?php echo base_url();?>assets/js/perfect-scrollbar.min.js"></script>
+ <script>
+var ps = new PerfectScrollbar('#sidebar');
+ </script>
+ <!-- custom js -->
+ <script src="<?php echo base_url();?>assets/js/chart_custom_style1.js"></script>
+ <script src="<?php echo base_url();?>assets/js/custom.js"></script>
+ </body>
+
+ </html>
