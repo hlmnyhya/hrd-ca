d8c58c6f8b23e2740a5903e7a2a2c50e6c98d7f4
Hilmanadi Yahya <hlmnyhya99@gmail.com>
2023-04-08 23:56:42 +0800

    REST API INSTALLED

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '93d851d1d2512c05979e1392022e72aebad5a1a4',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '93d851d1d2512c05979e1392022e72aebad5a1a4',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'chriskacerguis/codeigniter-restserver' => array(
+            'pretty_version' => '3.1.4',
+            'version' => '3.1.4.0',
+            'reference' => 'f386f4e0013daadaa213681a0a41af69aa81d9de',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../chriskacerguis/codeigniter-restserver',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
