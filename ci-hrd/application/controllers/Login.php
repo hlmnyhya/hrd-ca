53e3d44faba60558fcfc62c1c940a9f5776c80e6
MuhammadAldiyanoor <Mhmmdaldi877@gmail.com>
2023-04-14 02:29:57 +0800

    login

--- ci-hrd/application/controllers/Login.php
+++ ci-hrd/application/controllers/Login.php
@@ -20,7 +20,7 @@ class Login extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('templates/header');
+		// $this->load->view('templates/header');
         // $this->load->view('templates/sidebar');
         $this->load->view('login');
         //  $this->load->view('templates/footer');
