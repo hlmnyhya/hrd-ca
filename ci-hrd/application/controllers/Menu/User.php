de0624e8cdf210165043842a184b160d841e9f27
AHjazi <ahmdhjzi06@gmail.com>
2023-05-06 14:57:13 +0800

    sidebar

--- ci-hrd/application/controllers/Menu/User.php
+++ ci-hrd/application/controllers/Menu/User.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class MenuAkses extends CI_Controller {
+class User extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
@@ -22,7 +22,7 @@ class MenuAkses extends CI_Controller {
 	{
 		$this->load->view('templates/header');
         $this->load->view('templates/sidebar');
-        $this->load->view('Menu/menuakses');
+        $this->load->view('Menu/user');
         $this->load->view('templates/footer');
 	}
 }
--- ci-hrd/application/views/Menu/menuakses.php
+++
@@ -1,1 +0,0 @@
-<h2>MENU AKSES</h2>
